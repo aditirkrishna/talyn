9b5cc9de05755070f96380320fb38299c93eae9d
Aditi Ramakrishnan <aditirkrishna@gmail.com>
2025-06-25 06:56:07 +0530

    Add sample_space package initializer

---
+++ talyn/sample_space/__init__.py
@@ -0,0 +1,11 @@
+"""
+Talyn sample_space package: set operations and SampleSpace class.
+"""
+from .sample_space import SampleSpace, union, intersection, complement
+
+__all__ = [
+    "SampleSpace",
+    "union",
+    "intersection",
+    "complement",
+]
